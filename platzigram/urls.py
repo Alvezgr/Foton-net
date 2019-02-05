2fde36374576f3b4d28a5e0c77405df6dd2e3ec1
Gerardo <alvezpope@gmail.com>
2019-02-04 22:21:56 -0300

    sorted and say hi function created

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -4,5 +4,7 @@ from platzigram import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', views.hello_world)
+    path('hello-world/', views.hello_world),
+    path('sorted/', views.sort_integers),
+    path('hi/<str:name>/<int:age>', views.say_hi)
 ]
